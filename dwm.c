bfe9643d3be1a0ad1e7fcd4a40a8d8f8811d59d4
Bryan Bennett <bbenne10@gmail.com>
2014-01-07 09:53:34 -0500

    Change up the implementation of bstack a bit.
    This works better (but it's not perfect)

--- dwm.c
+++ dwm.c
@@ -443,45 +443,37 @@ attachstack(Client *c) {
     c->mon->stack = c;
 }
 
-static void
-bstack(Monitor *m) {
-    int w, h, mh, mx, tx, ty, tw;
-    unsigned int i, n;
-    Client *c;
+static void bstack(Monitor *m) {
+	int w, h, mh, mx, tx, ty, tw;
+	unsigned int i, n;
+	Client *c;
 
-    for(n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
-    if(n == 0)
-        return;
-    if(n > m->nmaster) {
-        mh = m->nmaster ? m->mfact * m->wh : 0;
-        //tw = (m->ww / (n - m->nmaster)) + globalborder/(n - m->nmaster);
-        tw = (m->ww / (n - m->nmaster)) ;
-        ty = m->wy + mh;
-    } 
-    else {
-        mh = m->wh;
-        tw = m->ww;
-        ty = m->wy;
-    }
-    for(i = mx = 0, tx = m->wx, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++) {
-        if(i < m->nmaster) {
-            w = (m->ww - mx) / (MIN(n, m->nmaster) - i);
-            resize(c, m->wx + mx, m->wy, w - (2 * c->bw), mh - (2 * c->bw), False);
-            mx += WIDTH(c) + globalborder;
-        } 
-        else {
-            w = (m->ww - tx) / (n - i);
-            h = m->wh - mh;
-            resize(c,
-                    tx,
-                    ty - globalborder,
-                    w - (2 * c->bw),
-                    h - (2 * c->bw) + globalborder,
-                    False);
-            if(tw != m->ww)
-                tx += WIDTH(c) + globalborder;
-        }
-    }
+	for(n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
+	if(n == 0)
+		return;
+	if(n > m->nmaster) {
+		mh = m->nmaster ? m->mfact * m->wh : 0;
+		tw = (m->ww + ((n-m->nmaster)-1)*globalborder) / (n - m->nmaster);
+		ty = m->wy + mh - globalborder;
+	} 
+	else {
+		mh = m->wh;
+		tw = m->ww;
+		ty = m->wy;
+	}
+	for(i = mx = 0, tx = m->wx, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++) {
+		if(i < m->nmaster) {
+			w = (m->ww - mx) / (MIN(n, m->nmaster) - i);
+			resize(c, m->wx + mx, m->wy, w - (2 * c->bw), mh - (2 * c->bw), False);
+			mx += WIDTH(c);
+		} 
+		else {
+			h = m->wh - mh + globalborder;
+			resize(c, tx, ty, tw - (2 * c->bw), h - (2 * c->bw), False);
+			if(tw != m->ww)
+				tx += WIDTH(c) + globalborder;
+		}
+	}
 }
 
 static void
